c35a94438148a7c52a5b6517154b8366ae736524
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2023-02-01 04:20:54 +0530

    brush dialoge implemented

--- app/src/main/java/com/example/mydrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/mydrawingapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.mydrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.mydrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView?=null
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawingView =findViewById(R.id.drawingView)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        val ibBrush : ImageButton=findViewById(R.id.ib_brush)
+        ibBrush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+
+
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog =Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+        val smallBtn= brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn= brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn= brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
+
+
